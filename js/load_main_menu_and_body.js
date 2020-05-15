51af3a2ad4d64d27a54f1122142a4e1c38cd28b8
tlukasova <tereza.lukasova@neuronsw.com>
2020-05-15 10:09:40 +0200

    shrinking navbar animation on scrolling added

--- js/load_main_menu_and_body.js
+++ js/load_main_menu_and_body.js
@@ -2,6 +2,7 @@ const main_bg_color = '#fcfaf2';
 const testcolor = '#123123';
 var language = window.localStorage.getItem('language');
 
+
 function set_bg_color() {
     $('body').css({backgroundColor: main_bg_color});
 }
@@ -10,16 +11,16 @@ function add_navbar() {
     $.get('templates/navbar.html', function (data, status) {
         //console.log(data);
         //$('#navbar-template-section').html(data);
-        var $data=$(data);
+        var $data = $(data);
 
-         if(language==='en') {
-             //console.log($data.find('#children_menu').text());
-             $data.find('#children_menu').text("Children");
-             $data.find('#adults_menu').text("Adults");
-             $data.find('#teachers_menu').text("Teachers");
-             $data.find('#navbarDropdown').text("Language");
-         }
-       $('#navbar-template-section').html($data.unwrap());
+        if (language === 'en') {
+            //console.log($data.find('#children_menu').text());
+            $data.find('#children_menu').text("Children");
+            $data.find('#adults_menu').text("Adults");
+            $data.find('#teachers_menu').text("Teachers");
+            $data.find('#navbarDropdown').text("Language");
+        }
+        $('#navbar-template-section').html($data.unwrap());
     });
     //var template_section = $('#navbar-template-section').load('templates/navbar.html');
     console.log("navbar loaded")
@@ -29,16 +30,16 @@ function add_navbar_other_path() {
     $.get('../templates/navbar_other_path.html', function (data, status) {
         //console.log(data);
         //$('#navbar-template-section').html(data);
-        var $data=$(data);
+        var $data = $(data);
 
-         if(language==='en') {
-             //console.log($data.find('#children_menu').text());
-             $data.find('#children_menu').text("Children");
-             $data.find('#adults_menu').text("Adults");
-             $data.find('#teachers_menu').text("Teachers");
-             $data.find('#navbarDropdown').text("Language");
-         }
-       $('#navbar-template-section').html($data.unwrap());
+        if (language === 'en') {
+            //console.log($data.find('#children_menu').text());
+            $data.find('#children_menu').text("Children");
+            $data.find('#adults_menu').text("Adults");
+            $data.find('#teachers_menu').text("Teachers");
+            $data.find('#navbarDropdown').text("Language");
+        }
+        $('#navbar-template-section').html($data.unwrap());
     });
     //var template_section = $('#navbar-template-section').load('templates/navbar.html');
     console.log("navbar loaded")
@@ -68,5 +69,29 @@ function expand_language_on_hover() {
         $('.nav-item.dropdown').addClass('show').children('div').addClass('show')
     } else {
         $('.nav-item.dropdown').removeClass('show').children('div').removeClass('show')
+    }
+}
+
+function navbar_resizing_scrolling() {
+    if (window.scrollY * 3 > window.innerHeight) {
+        if (navbarsize === 'large') {
+            // $('.navbar-brand').attr('style', 'padding-bottom: 5px; padding-top: 5px; height: 79px;');
+            $('.navbar-brand').addClass("smallnavbar").removeClass("bignavbarfinal");
+            setTimeout(() => {
+                $('.navbar-brand').removeClass("smallnavbar");
+            }, 1500);
+            navbarsize = 'small';
+            $('.navbar-brand').addClass("smallnavbarfinal");
+        }
+    } else {
+        //  $('.navbar-brand').attr('style', 'padding-top: 15px;padding-top: 15px; height: 99px;');
+        if (navbarsize === 'small') {
+            $('.navbar-brand').addClass("bignavbar").removeClass("smallnavbarfinal");
+            setTimeout(() => {
+                $('.navbar-brand').removeClass("bignavbar");
+            }, 1500);
+            navbarsize = 'large';
+            $('.navbar-brand').addClass("bignavbarfinal");
+        }
     }
 }
